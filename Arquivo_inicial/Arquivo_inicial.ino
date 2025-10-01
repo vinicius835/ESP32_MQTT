7f5abd6bcacc557fadc39aee8bab33055fcee3a2
VINÍCIUS ÂNGELO ALVES <vinicius_alves9@estudante.sesisenai.org.br>
2025-10-01 15:36:18 -0300

    Criada Função para scanear Redes Locais

--- Arquivo_inicial/Arquivo_inicial.ino
+++ Arquivo_inicial/Arquivo_inicial.ino
@@ -1,9 +1,28 @@
+#include <WiFi.h>
+scanLocalworks();
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  Serial.println("Iniciando Scan de redes Wi- Fi");
+  scanLocalworks();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+}
+
+void scanLocalworks(){
+  int number = WiFi.scanNetworks();
+  delay (500);
+  if(number == -1)  {
+    Serial.println("ERRO!. Não deu boa");
+  }
+  else{
+    for(int net = 0; net < number; net++){
+      Serial.printf("cont: %d", net);
+      Serial.printf("---- %d - %s | %d db\n",net,WiFi.SSID(net),WiFi.RSSI(net));
+      
+    }
+    // Serial.printf("Número de redes encontradas: %d\n", number);
+  }
 }
