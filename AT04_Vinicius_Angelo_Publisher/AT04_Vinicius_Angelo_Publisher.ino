1d9143b4cd91713a2b41cad9920ca80e63e8f5e4
vinicius835 <vinicius_alves9@estudante.sesisenai.org.br>
2025-10-16 17:21:05 -0300

    Atividade 04 - ATUALIZADA

---
+++ AT04_Vinicius_Angelo_Publisher/AT04_Vinicius_Angelo_Publisher.ino
@@ -0,0 +1,123 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+
+const String SSID = "Vi@@";
+const String PSWD = "angel0Vini";
+
+const String brokerUrl = "test.mosquitto.org";              //URL do broker (servidor)
+const int port = 1883;                                      //Porta do broker (servidor)
+
+const String topic_1 = "Carrinho/Cheios/1";
+WiFiClient espClient;  
+PubSubClient mqttClient(espClient);
+void scanLocalworks();
+void connectLocalworks();
+void connectBroker();
+// MQTT
+const byte pino_PIR = 19;
+int leitura_PIR = 0; //Variável para armazenar a leitura do sensor
+bool estadoSensor = false; //Variável para armazenar o estado do sensor
+//PIR
+
+String evento ="";
+//VARIAVEIS
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  pinMode(pino_PIR,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  leitura_PIR = analogRead(pino_PIR);
+  if(leitura_PIR == true){
+    Serial.println("movimento");
+    evento = "movimento";
+
+  } else if(leitura_PIR == false){
+    Serial.println("movimento");
+    evento = "nada_detectado";
+
+  }
+
+  StaticJsonDocument<200> doc
+    doc["evento"] = evento;
+     char buffer[200];
+    serializeJson(doc,buffer);
+
+//JSON - ENVIAR
+    mqttClient.publish("Carrinho/Cheios/1",buffer);
+    Serial.println("mensagem enviada");
+    Serial.println(buffer);
+    if(WiFi.status() != WL_CONNECTED){
+      Serial.print("Conexão Perdida\n");
+      connectLocalworks(); 
+    }
+    if(!mqttClient.connected()){
+      Serial.println("Erro de Conexão no Broker");
+      connectBroker();
+    }
+    mqttClient.loop();
+}
+void connectLocalworks(){
+  Serial.println("Iniciando conexão com rede WiFi");
+  
+  while (WiFi.status() != WL_CONNECTED){
+    WiFi.begin(SSID,PSWD);
+    Serial.print(".");
+    delay(2000);
+  }
+  Serial.println("\nConectado!");
+}
+void scanLocalworks(){
+  Serial.println("Iniciando Scan de redes Wi- Fi");
+  int number = WiFi.scanNetworks();
+  delay (500);
+  if(number == -1)  {
+    Serial.println("ERRO!. Não deu boa");
+  }
+  else{
+    for(int net = 0; net < number; net++){
+      Serial.printf("cont: %d", net);
+      Serial.printf("---- %d - %s | %d db\n",net,WiFi.SSID(net),WiFi.RSSI(net));
+      
+    }
+  
+  }
+}
+void connectBroker(){
+  Serial.println("Conectando ao broker");
+  mqttClient.setServer(brokerUrl.c_str(), port);
+  String userId = "ESP-alves9";
+  userId += String(random(0xffff), HEX);
+  
+  while(!mqttClient.connected()){
+    mqttClient.connect(userId.c_str());
+    Serial.println(".");
+    delay(200);
+  }
+    mqttClient.subscribe("Carrinho/Cheios/2");
+    mqttClient.setCallback(callback);
+    Serial.print("Conectado com sucesso!");
+  }
+  void callback(char* topic, byte* payload, unsigned long length){
+  String resposta = "";
+  for(int i = 0; i < length;i++){
+    // ele vai pegar um byte e  e transformar em letra
+    resposta += (char) payload[i];
+  }
+ DeserializationError error = deserializeJson(doc, resposta);
+  if(!error){
+    
+    String limiar = doc["comando"];
+    Serial.println("...");
+    if(limiar == "reset"){
+      analogWrite(pino_LED, 255);
+    }
+    else{
+      analogWrite(pino_LED, 0);
+    }
+  }
+}
